9783d72ee6de2223b91327cb9ebf47a9beaef39a
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-02-10 21:32:58 +0530

    Feat: Added Home Page
    1.Added Navbar
    2.Home section
    3. Categories
    4. Trending products

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    profilepage.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let profilepage = document.querySelector('.profile-page');
+
+document.querySelector('#profile-btn').onclick = () =>{
+    profilepage.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
